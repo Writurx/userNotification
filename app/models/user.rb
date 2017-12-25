5b04680b08ca8e68e10dd839ebabbcc3e33ede76
Writurx <DevLevAnd@gmail.com>
2017-12-25 14:03:29 +0200

    validates next fields: phone, email, sendToMess, sendToMail

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
 
   validates :firstName, length: {in: 2..20}
   validates :lastName, length: {in: 2..20}
-  validates :phone, format: { with: /((8|\+3)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}/, message: "incorrect phone" }
-  validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "incorrect email" }
+  validates :phone, if: -> { phone.present? }, format:{ :with => /[0-9\-()+]{6,16}/}
+  validates :email, if: -> { email.present? }, format:{ :with => /@/}
+  validates :phone, presence: { if: ->{email.blank?} }
+  validates :email, presence: { if: ->{phone.blank?} }
+  validates :sendToSms, presence: { if: ->{sendToMail.blank?}}
+  validates :sendToMail, presence: { if: ->{sendToSms.blank?}}
 end
